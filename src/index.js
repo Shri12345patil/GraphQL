2f79cb2d98f77b1935432e75ac887bc36aa7d514
Shri12345patil <jayshri.patil@sts.in>
2021-09-14 10:46:16 +0530

    fetch by use graphql done

--- src/index.js
+++ src/index.js
@@ -1,51 +1,13 @@
+// node src/index.js
 const { GraphQLServer } = require('graphql-yoga')
 const fetch = require('node-fetch')
 
-const baseURL = `http://localhost:8080`
+const baseURL = `http://127.0.0.1:5000/get`
 
 const resolvers = {
   Query: {
-    websites: () => {
-      return fetch(`${baseURL}/website`).then(res => res.json())
-    },
-    website: (parent, args) => {
-      const { id } = args
-      return fetch(`${baseURL}/website/${id}`).then(res => res.json())
-    }
-  },
-  Mutation: {
-    addWebsite: (parent, args) => {
-       const website = {
-        name: args.name,
-        url: args.url,
-      }
-	  return fetch(`${baseURL}/website`, { 
-			method: 'POST', 
-			body: JSON.stringify(website),
-			headers: { 'Content-Type': 'application/json' }
-		}).then(res => res.text());
-    },
-	updateWebsite: (parent, args) => {
-       const website = {
-		id: args.id,
-        name: args.name,
-        url: args.url,
-      }
-	  return fetch(`${baseURL}/website`, { 
-			method: 'PUT', 
-			body: JSON.stringify(website),
-			headers: { 'Content-Type': 'application/json' }
-		}).then(res => res.text());
-    },
-	deleteWebsite: (parent, args) => {
-       const website = {
-		id: args.id
-      }
-	  return fetch(`${baseURL}/website`, { 
-			method: 'DELETE', 
-			body: JSON.stringify(website),
-			headers: { 'Content-Type': 'application/json' }
-		}).then(res => res.text());
+    Articles: () => {
+      return fetch(`${baseURL}`).then(res => res.json())
     }
   }
 }
