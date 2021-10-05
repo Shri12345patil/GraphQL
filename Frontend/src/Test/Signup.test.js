5d50f03a2d864c752626bf0776d7c21d03478d58
Shri12345patil <jayshri.patil@sts.in>
2021-10-05 21:41:20 +0530

    unit test update

--- Frontend/src/Test/Signup.test.js
+++ Frontend/src/Test/Signup.test.js
@@ -1,11 +1,10 @@
-// import React from 'react';
-// import { render, screen} from '@testing-library/react';
-// import Signup from '../Signup';
+import React from 'react';
+import { render} from '@testing-library/react';
+import Signup from '../Signup';
 
-// describe("App", () => {
-// test('renders learn react link', () => {
-//   render(<Signup />);
-//   screen.debug();
-// });
 
-// });
+ describe("StudentList", () => {
+test('renders learn react link', () => {
+    
+});
+});
--- Frontend/src/Test/StudentList.test.js
+++ Frontend/src/Test/StudentList.test.js
@@ -1,11 +1,26 @@
-// import React from 'react';
-// import { render, screen} from '@testing-library/react';
-// import StudentList from '../StudentList';
+import React from 'react';
+import { render} from '@testing-library/react';
+import StudentList from '../StudentList';
+import { useQuery } from "@apollo/client";
+import { useLocation}  from 'react-router-dom';
 
-// describe("App", () => {
+// jest.mock('react-router-dom');
+// jest.mock("@apollo/client");
+
+ describe("StudentList", () => {
 // test('renders learn react link', () => {
-//   render(<StudentList />);
-//   screen.debug();
+//     useQuery.mockReturnValue({
+//         data: null,
+//         loading: true,
+//         error: null,
+//     });
+//     const{ debug } = render(
+//         <StudentList/>
+//     );
+//     debug();
 // });
 
-// });
+test('renders learn react link', () => {
+    
+});
+});
